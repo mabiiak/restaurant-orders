2fd043703c90ecd15606537340627f4a04d7a515
mabiiak <mabiane.polniak@gmail.com>
2022-09-21 09:23:11 -0300

    feat: create func open_csv

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,37 @@
+from os.path import exists
+import csv
+import sys
+
+'''
+    - Qual o prato mais pedido por 'maria'?
+
+    - Quantas vezes 'arnaldo' pediu 'hamburguer'?
+
+    - Quais pratos 'joao' nunca pediu?
+
+    - Quais dias 'joao' nunca foi à lanchonete?
+'''
+
+def open_csv(path_to_file):
+    print(path_to_file)
+    if ".csv" not in path_to_file:
+        raise ValueError("Arquivo inválido")
+
+    if not exists(path_to_file):
+        print(f"Arquivo {path_to_file} não encontrado", file=sys.stderr)
+        return None
+    
+    with open(path_to_file, mode="r") as file:
+        file_infos = list(csv.reader(file))
+        return file_infos
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    '''
+        filtrar informaçoes
+
+        criar arquivo > result_file = 'data/mkt_campaign.txt'
+
+        salvar dados filtrados em csv
+    '''
+    open_csv(path_to_file)
