a28a1b1f72b166481a13d550205e7b3068c8d43b
mabiiak <mabiane.polniak@gmail.com>
2022-09-26 15:09:11 -0300

    refactor: fix reduces the complexity of functions

--- src/track_orders.py
+++ src/track_orders.py
@@ -15,8 +15,6 @@ class TrackOrders:
     def get_most_ordered_dish_per_customer(self, customer):
         data = self.data
         count = {}
-        bigger = 0
-        result = ''
 
         for index in data:
             if customer == data[index][0] and data[index][1] not in count:
@@ -24,50 +22,38 @@ class TrackOrders:
             elif customer == data[index][0] and data[index][1] in count:
                 count[data[index][1]] += 1
 
-        for item in count:
-            if count[item] > bigger:
-                bigger = count[item]
-                result = item
-
+        result = max(count, key=count.get)
         return result
 
     def get_never_ordered_per_customer(self, customer):
         data = self.data
-        all_plates = []
-        eaten = []
-        result = set()
+        all_plates = set()
+        eaten = set()
 
         for index in data:
             if data[index][1] not in all_plates:
-                all_plates.append(data[index][1])
+                all_plates.add(data[index][1])
 
             if customer == data[index][0] and data[index][1] not in eaten:
-                eaten.append(data[index][1])
+                eaten.add(data[index][1])
 
-        for plate in all_plates:
-            if plate not in eaten:
-                result.add(plate)
-
-        return result
+        never_eaten = all_plates.difference(eaten)
+        return never_eaten
 
     def get_days_never_visited_per_customer(self, customer):
         data = self.data
-        all_days = []
-        visited = []
-        result = set()
+        all_days = set()
+        visited = set()
 
         for index in data:
             if data[index][2] not in all_days:
-                all_days.append(data[index][2])
+                all_days.add(data[index][2])
 
             if customer == data[index][0] and data[index][2] not in visited:
-                visited.append(data[index][2])
+                visited.add(data[index][2])
 
-        for plate in all_days:
-            if plate not in visited:
-                result.add(plate)
-
-        return result
+        never_visited = all_days.difference(visited)
+        return never_visited
 
     def get_busiest_day(self):
         pass
